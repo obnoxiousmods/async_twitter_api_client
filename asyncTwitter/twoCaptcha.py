1c13b7d10ed9101fa8d1e007ebf717b03bd6d8c6
oewpgjewoghjewoghewg <eojweogijewogewjg@Oijgweiojwgiewjg.ewijewgi>
2024-04-25 14:44:30 -0700

    gg

--- asyncTwitter/twoCaptcha.py
+++ asyncTwitter/twoCaptcha.py
@@ -4,103 +4,6 @@ import anyio
 from httpx import AsyncClient
 
 
-class CaptchaQueue:
-    def __init__(self, main, max_available_captchas):
-        self.main = main
-        self.max_available_captchas = max_available_captchas
-        self.queue_send, self.queue_recv = trio.open_memory_channel(
-            max_available_captchas
-        )
-        self.capacityLimiter = trio.CapacityLimiter(max_available_captchas)
-
-    async def fill_queue(self):
-        """
-        This method fills the captcha queue up to its maximum capacity by
-        submitting captcha creation tasks and adding the results to the queue.
-        """
-        for _ in range(self.max_available_captchas):
-            await self.submit_captcha_task()
-
-    async def get_captcha(self):
-        """
-        Retrieves a captcha from the queue. If the queue is empty, this will
-        wait until a captcha becomes available. After retrieving a captcha,
-        it submits a new task to ensure the queue is always filled.
-        """
-        captcha_result = await self.queue_recv.receive()
-
-        # self.main.general.logger.info(f"Retrieved captcha from queue: {captcha_result}")
-
-        return captcha_result
-
-    async def submit_captcha_task(self):
-        """
-        Submits a captcha creation task and waits for its completion before
-        adding the result to the queue.
-        """
-        async with self.capacityLimiter:
-            v2Resp = await self.main.captcha.createTask(
-                websiteKey=self.main.general.config.get(
-                    "onlyFansWebsiteRecaptchaV2Key", None
-                ),
-                websiteUrl="https://onlyfans.com/",
-                method="RecaptchaV2TaskProxyless",
-                isEnterprise=False,
-                isInvisible=False,
-            )
-
-            if not v2Resp.get("success", False):
-                self.main.general.logger.error(
-                    f"Failed to create captcha task: {v2Resp.get('data', 'Unknown error')}"
-                )
-                return False
-
-            v2TaskId = v2Resp.get("taskId", None)
-
-            self.main.general.logger.info(f"Created captcha task: {v2TaskId}")
-
-            v2CaptchaResult = await self.main.captcha.checkTaskUntilFinished(v2TaskId)
-
-            # Send the captcha result to the queue
-            # self.main.general.logger.info(f"Adding captcha to queue: {v2CaptchaResult}")
-            await self.queue_send.send(
-                {
-                    "success": True,
-                    "v2": v2CaptchaResult,
-                }
-            )
-
-    async def maintain_queue(self):
-        """
-        Keeps the queue filled with captcha tasks. This is a background task
-        that continuously checks the queue's capacity and fills it as necessary.
-        """
-
-        while True:
-            async with trio.open_nursery() as self.nursery:
-                if (
-                    self.capacityLimiter.borrowed_tokens < self.max_available_captchas
-                    and self.queue_send.statistics().current_buffer_used
-                    < self.max_available_captchas
-                ):
-                    newCaptchasToCreate = (
-                        self.max_available_captchas
-                        - self.capacityLimiter.borrowed_tokens
-                    )
-
-                    self.main.general.logger.info(
-                        f"Queue is low, creating {newCaptchasToCreate} new captchas | {self.capacityLimiter.borrowed_tokens} / {self.max_available_captchas} tokens used."
-                    )
-
-                    for _ in range(newCaptchasToCreate):
-                        self.nursery.start_soon(self.submit_captcha_task)
-                else:
-                    self.main.general.logger.info(
-                        f"Queue is full, {self.capacityLimiter.borrowed_tokens} / {self.max_available_captchas} tokens used. Sleeping..."
-                    )
-            await trio.sleep(5)
-
-
 class TwoCaptcha:
     def __init__(self, main: object, apiKey: str):
         self.main = main
@@ -115,6 +18,30 @@ class TwoCaptcha:
             proxies="http://127.0.0.1:8866",
             headers={"Content-Type": "application/json"},
         )
+
+    async def checkBalance(self):
+        resp = await self.client.post(
+            f"{self.baseUrl}/getBalance",
+            json={
+                "clientKey": self.apiKey,
+            },
+        )
+
+        respJson = resp.json()
+
+        balance = respJson.get("balance", None)
+
+        if not balance:
+            return {
+                "success": False,
+                "error": "Failed to get balance",
+                "data": respJson,
+            }
+
+        return {
+            "success": True,
+            "balance": balance,
+        }
 
     async def checkTaskUntilFinished(
         self, taskId: str, sleepTime: int = 9, maxRetries: int = 15
